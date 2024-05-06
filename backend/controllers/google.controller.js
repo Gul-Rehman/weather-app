aa9d36f2a437389d6388ac2bfabea8d33e332a5d
Gul Rehman <60466839+Gul-Rehman@users.noreply.github.com>
2024-05-06 12:57:59 +0500

    code cleanup

--- backend/controllers/google.controller.js
+++ backend/controllers/google.controller.js
@@ -19,10 +19,7 @@ const authorizationUrl = oauth2Client.generateAuthUrl({
 
 const getAccessTokenHandler = async (req, res, next) => {
   try {
-    // Get the access token from the OAuth2 client
     const accessToken = oauth2Client.credentials.access_token;
-
-    // Return the access token in the response
     res.status(200).json({ access_token: accessToken });
   } catch (err) {
     next(err);
@@ -44,7 +41,6 @@ const googleCallBackHandler = async (req, res, next) => {
     oauth2Client.setCredentials(tokens);
     console.log("tokens : ", tokens);
     res.redirect(`http://localhost:3000/`);
-    // res.json({ tokens });
   } catch (err) {
     next(err);
   }
@@ -52,27 +48,21 @@ const googleCallBackHandler = async (req, res, next) => {
 
 const logout = async (req, res) => {
   try {
-    const accessToken = req.headers.authorization; // Assuming the access token is stored on the client side
+    const accessToken = req.headers.authorization;
     if (!accessToken) {
-      // If access token is not available, return an error response
       return res.status(400).json({ error: "Access token is missing" });
     }
 
-    // Revoke the access token by making a request to Google's token revocation endpoint
     await axios.post("https://accounts.google.com/o/oauth2/revoke", null, {
       params: {
         token: accessToken,
       },
     });
 
-    // Clear any session or token stored on the client side
-    // For example, clear cookies, localStorage, or sessionStorage
     oauth2Client.setCredentials(null);
 
-    // Return a success response indicating logout was successful
     return res.status(200).json({ message: "Logout successful" });
   } catch (error) {
-    // If an error occurs during logout, return an error response
     console.error("Error during logout:", error);
     return res.status(500).json({ error: "Internal server error" });
   }
